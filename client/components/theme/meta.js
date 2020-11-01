ca4e49befe5fb3659a51f73c6b4414b9668800ed
DVitsme <derrick.valentine@gmail.com>
2020-10-31 21:53:21 -0400

    I wont forget you nprogress...probably

---
+++ client/components/theme/meta.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    {
+      //<link rel="stylesheet" type="text/css" href="/static/nprogress.css" /> Dont Forget Me Set Me up Later
+    }
+    <link rel="shortcut icon" href="/images/favicon/nficon2016.ico" />
+    <link
+      type="text/css"
+      href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,300&display=swap"
+      rel="stylesheet"
+    />
+
+    <title>Netflix Clone || </title>
+  </Head>
+);
+
+export default Meta;
