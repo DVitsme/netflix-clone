4f58548c97fc1bdc5d205965dd6dc08d0182eb80
DVitsme <derrick.valentine@gmail.com>
2020-11-02 16:03:12 -0500

    removed unnecessary div tag

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,12 @@
-import styled from 'styled-components'
+import React from "react";
+import StoryCard from "../components/storyCard/index.jsx";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const Home = () => {
+  return (
+    <React.Fragment>
+      <StoryCard />
+    </React.Fragment>
+  );
+};
 
-export default function Home() {
-  return <Title>My page</Title>
-}
+export default Home;
