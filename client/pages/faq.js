7204b8e96a342508bae5a8b14191054661ac4d1e
DVitsme <derrick.valentine@gmail.com>
2020-11-02 16:01:23 -0500

    all added for setup for slugify for footer

---
+++ client/pages/faq.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const faq = () => {
+  return (
+    <div>
+      <Link href="/">
+        <a>Go Back</a>
+      </Link>
+      <h1>Im just here to test load time</h1>
+    </div>
+  );
+};
+
+export default faq;
